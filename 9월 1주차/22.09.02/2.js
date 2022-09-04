08d6d5ee1a8047dd4a908089139e572c21f85721
ParkGeunSu <geunsu902@gmail.com>
2022-09-04 23:38:27 +0900

    feat : ScrollLeft Page
    마우스휠, 바운스기능 추가

--- 9월 1주차/22.09.02/2.js
+++ 9월 1주차/22.09.02/2.js
@@ -3,15 +3,22 @@
 
 $(function () {
 
-    $("article").on('mousewheel',function(e){		
-        var wheelDelta = e.originalEvent.wheelDelta;		
-        if(wheelDelta > 0){			
-            console.log("up");			
-            $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());		
-        }else{		console.log("down");			
-        $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());	
-    	}});
-    
+  var w;
+  var h;
+  $(window).resize(function(){
+    w = $(window).width();
+        h = $(window).height();
+    $('article').width(w);
+    $('article').height(h);
+  });
+  $(window).trigger('resize');
+
+$('.lnb > li').click(function(){
+  var i = $(this).index();
+  var nowLeft = $('.section').eq(i).offset().left;
+  $('html,body').stop().animate({'scrollLeft':nowLeft},1300);
+  return false;
+});
 
 $(window).scroll(function () {
     
@@ -38,15 +45,16 @@ if(sct >= $("article").eq(0).offset().left){
  });
 
  $('article').mousewheel(function(event,d){
-    if(d>0){ //휠을 위로 올릴 때
-        let prev = $(this).prev().offset().top;
-        $('html,body').stop().animate({scrollLeft : prev},1000,'easeOutBounce')
-
+   
+    if(d>0){
+      var prev = $(this).prev().offset().left;
+      $('html,body').stop().animate({scrollLeft:prev},1300,'easeOutBounce');
+   
     }
-    if(d<0){ //휠을 아래로 내릴 때
-        let next = $(this).next().offset().top;
-        $('html,body').stop().animate({scrollLeft : next},1000,'easeOutBounce'   )
-
+    if(d<0){
+      var next = $(this).next().offset().left;
+      $('html,body').stop().animate({scrollLeft:next},1300,'easeOutBounce');
+     
     }
 });
 
