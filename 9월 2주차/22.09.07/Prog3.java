c5c6694bc04cf0ef601ca892c8f2fbda0031e514
ParkGeunSu <geunsu902@gmail.com>
2022-09-07 14:44:13 +0900

    feat : 22.09.07  개발 공부
    HTML,CSS, JavaScript :
    막대 원형 그래프, 자동 타이핑 기능 구현

---
+++ 9월 2주차/22.09.07/Prog3.java
@@ -0,0 +1,15 @@
+public class Prog3 {
+
+    // 공백 있는 문자열
+    private static String Space = "게임 하고싶다.";
+
+    public static void main(String[] args) {
+
+        System.out.println("\" \" 로 분리");
+        String[] ParsedSpace = Space.split(" ");
+        printarr(ParsedSpace);
+
+        System.out.println("\\\\s 로 분리");
+        ParsedSpace = Space.split("\\s");
+        printarr(ParsedSpace);
+    }
