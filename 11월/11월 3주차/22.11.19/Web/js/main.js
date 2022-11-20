f8a9081a0e711339befebf8afe266457d175b61f
rlj_su <geunsu902@gmail.com>
2022-11-20 20:17:23 +0900

    feat : 22.11.20 VS-Code
    Section4 추가

--- 11월/11월 3주차/22.11.19/Web/js/main.js
+++ 11월/11월 3주차/22.11.19/Web/js/main.js
@@ -61,7 +61,7 @@ $(function(){
     var b3 = $(".s3_title p").offset().top;
     var b4 = $(".s3_table li").offset().top;
 
-    console.log(a1,a2,a3,a4);
+    console.log(b1,b2,b3,b4);
 
     $(window).scroll(function(){
         var sct = $(this).scrollTop(); //스크롤의 위치값
@@ -88,6 +88,39 @@ $(function(){
             setTimeout(function(){
                 $(".s3_table li").eq(4).addClass("slide");
             },1200)
+        }
+    });
+
+    var c1 = $(".s4_title img").offset().top;
+    var c2 = $(".s4_title h2").offset().top;
+    var c3 = $(".s4_title p").offset().top;
+    var c4 = $(".s4_table li").offset().top;
+
+    console.log(c1,c2,c3,c4);
+
+    $(window).scroll(function(){
+        var sct = $(this).scrollTop(); //스크롤의 위치값
+        if(c1 < sct + 700){
+            $(".s4_title img").addClass("slide");
+        }
+        if(c2 < sct + 700){
+            $(".s4_title h2").addClass("slide");
+        }
+        if(c3 < sct + 700){
+            $(".s4_title p").addClass("slide");
+        }
+        if(c4 < sct + 700){
+            $(".s4_table li").eq(0).addClass("slide");
+            setTimeout(function(){
+                $(".s4_table li").eq(1).addClass("slide");
+            },300)
+            setTimeout(function(){
+                $(".s4_table li").eq(2).addClass("slide");
+            },600)
+            setTimeout(function(){
+                $(".s4_table li").eq(3).addClass("slide");
+            },900)
+           
         }
     });
     
