d9dc8a4959dd2b60f4ca02accf166636855704e4
rlj_su <geunsu902@gmail.com>
2022-12-05 13:41:33 +0900

    feat : 22.12.05 VS-Code
    3D Scroll 완성

--- 12월/12월 1주차/22.12.02/3dscroll/js/init.js
+++ 12월/12월 1주차/22.12.02/3dscroll/js/init.js
@@ -1,5 +1,11 @@
 $(function(){
-
-  
-
+    $(window).scroll(function(){
+        var sct = $(this).scrollTop();
+        
+       
+    for(var i = 0; i < 5; i++){
+            $("section > article").eq(i).css({ transform: "translateZ(" + (-5000 * i + sct) + "px)"});
+        }
+ 
+});
 });
